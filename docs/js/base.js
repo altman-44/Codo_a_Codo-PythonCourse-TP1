fc0a749a7f815302994dd25cc57b60695209f81b
altman-44 <altman.nicolas44@gmail.com>
2021-05-19 00:41:36 -0300

    Home was empty and some pages had mistakes regarding style

--- docs/js/base.js
+++ docs/js/base.js
@@ -1,13 +1,14 @@
 // Ejecutar cuando el contenido del DOM haya cargado
 document.addEventListener('DOMContentLoaded', () => {
     setMainHeight()
+    setAnchorLinksBehavior()
 
     // Como mínimo, el height del elemento main y el del header tiene que
     // ocupar toda la pantalla para que no queden espacios vacíos
     function setMainHeight() {
         let headerHeight = document.querySelector('header').clientHeight
         document.querySelector('main').style.minHeight =
-            (window.innerHeight - headerHeight) + 'px'
+            window.innerHeight - headerHeight + 'px'
         const menu = document.querySelector('.menu')
         if (window.screen.width < 768) {
             menu.style.minHeight = window.innerHeight + 'px'
@@ -15,6 +16,21 @@ document.addEventListener('DOMContentLoaded', () => {
         } else {
             menu.style.minHeight = '100%'
         }
+    }
+
+    function setAnchorLinksBehavior() {
+        document.querySelectorAll('a').forEach((aElement) => {
+            const splitted = aElement.href.split('#')
+            if (splitted.length > 1)
+                aElement.addEventListener('click', (e) => {
+                    e.preventDefault()
+                    const target = document.querySelector(`#${splitted[1]}`)
+                    target.scrollIntoView({
+                        behavior: 'smooth',
+                        block: 'center'
+                    })
+                })
+        })
     }
 
     // Detectar cuando la ventana cambia de tamaño
